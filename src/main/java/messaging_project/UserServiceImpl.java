0dc590a216dcf1dbcd9f9d41be36efbc338b27eb
domenikon <domenikon@gmail.com>
2018-08-29 22:48:56 +0300

    added methods

--- src/main/java/messaging_project/UserServiceImpl.java
+++ src/main/java/messaging_project/UserServiceImpl.java
@@ -1,4 +1,50 @@
 package messaging_project;
 
+import com.company.User;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Scanner;
+
 public class UserServiceImpl {
+    Scanner scanner = new Scanner(System.in);
+    com.company.User user = new User();
+
+    public void addUser() throws FileNotFoundException {
+        System.out.println("Enter email: ");
+        String email = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+        System.out.println("Enter full name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter age: ");
+        int age = scanner.nextInt();
+        PrintWriter writer = new PrintWriter(email + ".txt");
+        writer.println(email);
+        writer.println(password);
+        writer.println(name);
+        writer.println(age);
+        writer.close();
+    }
+
+    public void login() throws IOException {
+        System.out.println("Enter email: ");
+        String email = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+
+        Path filePath = Paths.get(email + ".txt");
+        List<String> records = Files.readAllLines(filePath);
+        if (records.get(0).equals(email) && records.get(1).equals(password)){
+            System.out.println("Welcome " + records.get(2));
+        }else {
+            System.out.println("Email or password incorrect!");
+        }
+    }
 }

4d4882f9eeffb49db98e8bc87f0b4fb219896202
domenikon <domenikon@gmail.com>
2018-08-29 22:54:15 +0300

    added methods

--- src/main/java/messaging_project/Main.java
+++ src/main/java/messaging_project/Main.java
@@ -1,7 +1,28 @@
 package messaging_project;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        UserServiceImpl userService = new UserServiceImpl();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose option: ");
+        System.out.println("Sign up! ");
+        System.out.println("Login. ");
+        int option = scanner.nextInt();
+
+        switch (option){
+
+            case 1:
+                userService.addUser();
+                break;
+            case 2:
+                userService.login();
+        }
 
     }
 }

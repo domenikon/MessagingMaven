9fc8c774fb50c92f10cea6ef1981611669256e83
domenikon <domenikon@gmail.com>
2018-08-30 01:30:58 +0300

    fixes

--- src/main/java/messaging_project/UserServiceImpl.java
+++ src/main/java/messaging_project/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -51,14 +52,16 @@ class UserServiceImpl {
         String email = scanner.nextLine();
         Path filePath = Paths.get(email + ".txt");
 
-        if(!filePath.getFileName().equals(email)){
+        if(!filePath.getFileName().toString().equals(email + ".txt" ) ){
             System.out.println("No such user! Try again.");
             login();
         }else {
             System.out.println("Enter password: ");
             String password = scanner.nextLine();
           //  Path filePath1 = Paths.get(email + ".txt");
-            List<String> records = Files.readAllLines(filePath);
+
+           List<String> records = Files.readAllLines(filePath);
+
             if (records.get(0).equals(email) && records.get(1).equals(password)) {
                 System.out.println("Welcome " + records.get(2));
             } else {

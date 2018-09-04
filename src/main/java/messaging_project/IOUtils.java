30c316eb42f503adbfbd56be33530582a3e25454
Denis T <domenikon@gmail.com>
2018-09-04 19:21:23 +0300

    some small changes, you don't need to know

--- src/main/java/messaging_project/IOUtils.java
+++ src/main/java/messaging_project/IOUtils.java
@@ -65,15 +65,15 @@ public class IOUtils {
     }
 
     public void writeMessageToUser(String message, String fileName) throws IOException {
-        if (!conversationFileExist(fileName)) {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-            writer.append(message);
-            writer.close();
-        } else {
+//        if (!conversationFileExist(fileName)) {
+//            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+//            writer.append(message);
+//            writer.close();
+//        } else {
             FileWriter fileWrite = new FileWriter(fileName, true);
             fileWrite.write("\n" + message);
             fileWrite.close();
-        }
+//        }
         ;
     }
 
